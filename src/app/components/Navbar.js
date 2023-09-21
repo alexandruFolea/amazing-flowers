2ea2c125faf323416a5236920a01ae5e3d11f799
Alex <alexandrufolea2@gmail.com>
2023-09-21 15:26:44 -0400

    sdfh

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+	return (
+		<div>
+			<ul>
+				<li>one</li>
+				<li>two</li>
+				<li>three</li>
+			</ul>
+		</div>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar';
+
 export default function Home() {
 	return (
 		<main>
-			<h1>hi</h1>
+			<Navbar />
 		</main>
 	);
 }
